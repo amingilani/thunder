6961c22532607274352f3695abf7e3aa5005a963
Amin Shah Gilani <amin@gilani.me>
2018-10-20 03:53:49 +0500

    Don't be interactive ufw script

--- scripts/setup_firewall.sh
+++ scripts/setup_firewall.sh
@@ -2,9 +2,9 @@
 
 # install the essentials
 
-ufw allow ssh
-ufw allow 8333
-ufw allow 8333
-ufw allow 9735
-ufw allow out 9735
-ufw enable
+ufw allow --force ssh
+ufw allow --force 8333
+ufw allow --force 8333
+ufw allow --force 9735
+ufw allow --force out 9735
+ufw --force enable

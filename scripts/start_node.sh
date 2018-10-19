0bbb749ea6bfc5829853050337be76a2843e4d83
Amin Shah Gilani <amin@gilani.me>
2018-10-20 02:28:18 +0500

    Fix node not starting

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tmux new-session -d 'docker-compose up ..'
+tmux new-session -d 'docker-compose up -f ../docker-compose.yml up'
 
 echo 'Tmux session now running, run `tmux a` to connect.'

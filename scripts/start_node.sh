3eb644a9475664bd0e38c823154b9baf96831c45
Amin Shah Gilani <amin@gilani.me>
2018-10-20 02:31:54 +0500

    Typo

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
-tmux new-session -d 'docker-compose up -f ../docker-compose.yml up'
+tmux new-session -d 'docker-compose -f ../docker-compose.yml up'
 
 echo 'Tmux session now running, run `tmux a` to connect.'

963cde1ef32de0342fc63c5453a7792e2230e03d
Amin Shah Gilani <amin@gilani.me>
2018-10-20 01:43:59 +0500

    Fix file generating docker config

--- create_docker_compose_config.sh
+++ create_docker_compose_config.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-LOCAL_IP=ip -o route get 8.8.8.8 | sed -e 's/^.* src \([^ ]*\) .*$/\1/'
+LOCAL_IP=$(ip -o route get 8.8.8.8 | sed -e 's/^.* src \([^ ]*\) .*$/\1/')
 NODE_ALIAS="gthunder"
+
+erb node_alias=$NODE_ALIAS local_ip=$LOCAL_IP docker-compose.yml.erb > docker-compose.yml

5fecf810e91e19da81d0075edb4b558ce41dc31d
Amin Shah Gilani <amin@gilani.me>
2018-10-19 12:09:55 +0500

    Talk more

--- remote.sh
+++ remote.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
 # install the essentials
+
+echo "Installing essentials"
+
 apt-get update && apt-get install -y git tmux wget unzip
 
 ZIPFILE=thunder.zip
 UNZIPDIR="thunder-master"
 
+echo "Downloading Gilani's Thunder"
 wget https://github.com/amingilani/thunder/archive/master.zip -O $ZIPFILE
+
+echo "Setting up code"
 unzip $ZIPFILE
+rm $ZIPFILE
 
 cd $UNZIPDIR
 
 tmux new-session -d './run.sh'
 
-echo 'Tmux session now running'
+echo 'Tmux session now running, run `tmux -a` to connect.'
 
 cd

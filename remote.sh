1c7172616710d01ed94d08100e7f18be74651202
Amin Shah Gilani <amin@gilani.me>
2018-10-20 01:14:04 +0500

    Clone instead of downloading the zipfile

--- remote.sh
+++ remote.sh
@@ -6,17 +6,10 @@ echo "Installing essentials"
 
 apt-get update && apt-get install -y git tmux wget unzip
 
-ZIPFILE=thunder.zip
-UNZIPDIR="thunder-master"
 
 echo "Downloading Gilani's Thunder"
-wget https://github.com/amingilani/thunder/archive/master.zip -O $ZIPFILE
-
-echo "Setting up code"
-unzip $ZIPFILE
-rm $ZIPFILE
-
-cd $UNZIPDIR
+git clone https://github.com/amingilani/thunder
+cd thunder
 
 tmux new-session -d './run.sh'
 
